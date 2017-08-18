ad471dc97a63db2ea407804e6b65414c672b6946
Eric Wen <eericwen@icloud.com>
2017-08-18 16:43:54 +0800

    modify url root of API

--- server/weblog.js
+++ server/weblog.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Config = require('./config');
+const Queue = require('./queue');
 
 const app = express();
+const router = express.Router();
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
 app.use(express.static('frontend', {extensions:['html']}))
 
-const Config = require('./config')
-const Queue = require('./queue')
-
 const logQueue = new Queue(Config.maxLogNum);
 
-app.post('/log', function (req, res) {
+router.post('/logs', function (req, res) {
 	let logStr = req.body.str;
 	if (logStr.length > 0) {
 		logQueue.enqueue(req.body.str);
@@ -21,12 +21,14 @@ app.post('/log', function (req, res) {
 	res.json({'status':'success'});
 });
 
-app.get('/log', function (req, res) {
+router.get('/logs', function (req, res) {
 	res.json({
 		'status': 'success',
 		'data': logQueue.toArray(),
 	});
 });
+
+app.use('/api', router);
 
 app.listen(Config.port, function() {
 	console.log(`app listening on port ${Config.port}`);
